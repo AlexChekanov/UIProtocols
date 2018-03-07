2ddab744513195b31ceb23d0802f25356d882d34
Alexey Chekanov <chekanov@hotmail.com>
2018-03-07 15:51:40 -0500

    Initial Commit

---
+++ UIProtocols/UIProtocols.h
@@ -0,0 +1,19 @@
+//
+//  UIProtocols.h
+//  UIProtocols
+//
+//  Created by Alexey Chekanov on 3/7/18.
+//  Copyright © 2018 Tips & Tricks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIProtocols.
+FOUNDATION_EXPORT double UIProtocolsVersionNumber;
+
+//! Project version string for UIProtocols.
+FOUNDATION_EXPORT const unsigned char UIProtocolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIProtocols/PublicHeader.h>
+
+
